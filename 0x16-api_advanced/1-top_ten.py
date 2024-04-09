02c0a57de86ac58cc966d64b47bd5a770a4e02d3
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-04-09 03:57:57 +0000

    API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module is for getting the first 10 hot posts for a certain subreddit.
+"""
+
+import requests
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'by u/qasqot79'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        top_ten(subreddit)
