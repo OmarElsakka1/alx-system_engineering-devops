a8b305f4f4c41b535c106d5d4ba749e709e10c61
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-04-09 03:47:17 +0000

    0-sub

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module is for getting the number of subscribers of a certain subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'by u/qasqot79'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(num_subscribers)
