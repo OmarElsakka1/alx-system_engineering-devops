98aa5734c3575da5a754fea43d0f682323099c3c
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-04-12 03:05:07 +0200

    1-top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,24 +3,29 @@
 This module is for getting the first 10 hot posts for a certain subreddit.
 """
 
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Read reddit API and return top 10 hotspots """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        list_titles = r.json()['data']['children']
-        for a in list_titles[:10]:
-            print(a['data']['title'])
+    """
+    Getting the top 10 of thhe given subreddit
+    """
+
+    headers = {
+            "Accept": "*/*",
+            "User-Agent": "ALX student script",
+    }
+    api_url = 'https://www.reddit.com/r'
+    type_ = 'hot'
+    url = '{}/{}/{}.json?raw_json=1&limit=10'.format(
+        api_url, subreddit, type_
+    )
+    res = requests.get(url, headers=headers)
+    if not res.ok:
+        print(None)
     else:
-        return (print("None"))
+        data = res.json()
+        posts = data["data"]["children"]
+        for post in posts:
+            title = post['data']["title"]
+            print(title)
